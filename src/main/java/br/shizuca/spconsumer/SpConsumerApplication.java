7895f6cd4776a8333e5a6b47958f63e7a0965a57
Bernardo Duarte <bernardoduartes@gmail.com>
2023-12-15 10:29:55 -0300

    build : add maven dependencies

---
+++ src/main/java/br/shizuca/spconsumer/SpConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.shizuca.spconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpConsumerApplication.class, args);
+	}
+
+}
